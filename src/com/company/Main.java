beb72fabf50a4d8c8d327ea207198bc8edc8a85b
Ankit Amonkar <ankit.amonkar@gmail.com>
2019-09-13 12:02:06 -0400

    Goodbye!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Goodbye");
+	//System.out.println("Goodbye");
+	//double x = Math.random();   //random number [0,1]
+        //double x = Math.random()*10; //random int [0.10]
+        //int x = (int)(Math.random()*10 + 10);  //random int [10,20]
+        //int x = (int)(Math.random()*8+1);
+        //int x = (int)(Math.random()*6);
+        //int x = (int)(Math.random()*2);
+        //int x = (int)(Math.random()*6+1) + (int)(Math.random()*6+1);
+        System.out.println(x);
     }
 }
